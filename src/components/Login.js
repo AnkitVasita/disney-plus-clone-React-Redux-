16e76895d91f900f821a5d64590937b0fc8a3fca
AnkitVasita <vasitaankit97@gmail.com>
2021-05-10 14:41:51 +0530

    fornt change

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,89 +1,106 @@
-import React from "react";
 import styled from "styled-components";
 
-const Login = () => {
+const Login = (props) => {
   return (
     <Container>
-      <ContentBox>
-        <LogoOne src="/images/cta-logo-one.svg" alt="" />
-        <SignUp>GET ALL THERE</SignUp>
-        <Description>
-          Get Premier Access to Raya and the Last Dragon for an additional fee
-          with a Disney+ subscription. As of 03/26/21, the price of Disney+ and
-          The Disney Bundle will increase by $1.
-        </Description>
-        <LogoTwo src="/images/cta-logo-two.png" alt="" />
-      </ContentBox>
+      <Content>
+        <CTA>
+          <CTALogoOne src="/images/cta-logo-one.svg" alt="" />
+          <SignUp>GET ALL THERE</SignUp>
+          <Description>
+            Get Premier Access to Raya and the Last Dragon for an additional fee
+            with a Disney+ subscription. As of 03/26/21, the price of Disney+
+            and The Disney Bundle will increase by $1.
+          </Description>
+          <CTALogoTwo src="/images/cta-logo-two.png" alt="" />
+        </CTA>
+        <BgImage />
+      </Content>
     </Container>
   );
 };
 
-export default Login;
-
-const Container = styled.div`
-  position: relative;
-  height: calc(100vh - 70px);
-  display: flex;
-  align-items: center;
-  justify-content: center;
-
-  &:before {
-    background-position: top;
-    background-size: cover;
-    background-repeat: no-repeat;
-    background: url("images/login-background.jpg") center center / cover
-      no-repeat fixed;
-    content: "";
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    opacity: 0.7;
-    z-index: -1;
-  }
-`;
-
-const ContentBox = styled.div`
-  max-width: 650px;
-  padding: 80px 40px;
-  width: 90%;
+const Container = styled.section`
+  overflow: hidden;
   display: flex;
   flex-direction: column;
-  /* margin-top: 70px; */
-  align-items: center;
-`;
-
-const LogoOne = styled.img``;
-const LogoTwo = styled.img`
-  width: 90%;
-`;
-
-const Description = styled.p`
-  color: hsla(0, 0%, 95.3%, 1);
-  font-size: 11px;
   text-align: center;
-  margin: 0 0 24px;
-  line-height: 1.5;
-  letter-spacing: 1.5px;
+  height: 100vh;
 `;
+
+const Content = styled.div`
+  margin-bottom: 10vw;
+  width: 100%;
+  position: relative;
+  min-height: 100vh;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  padding: 80px 40px;
+  height: 100%;
+`;
+
+const BgImage = styled.div`
+  height: 100%;
+  background-position: top;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-image: url("/images/login-background.jpg");
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  z-index: -1;
+`;
+
+const CTA = styled.div`
+  max-width: 650px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const CTALogoOne = styled.img`
+  margin-bottom: 12px;
+  max-width: 600px;
+  min-height: 1px;
+  display: block;
+  width: 100%;
+`;
+
 const SignUp = styled.a`
   font-weight: bold;
   color: #f9f9f9;
   background-color: #0063e5;
-  margin-top: 8px;
   margin-bottom: 12px;
   width: 100%;
   letter-spacing: 1.5px;
   font-size: 18px;
   padding: 16.5px 0;
-  text-align: center;
   border: 1px solid transparent;
   border-radius: 4px;
-  cursor: pointer;
-  transition: all 250ms;
-
   &:hover {
     background-color: #0483ee;
   }
 `;
+
+const Description = styled.p`
+  color: hsla(0, 0%, 95.3%, 1);
+  font-size: 11px;
+  margin: 0 0 24px;
+  line-height: 1.5;
+  letter-spacing: 1.5px;
+`;
+
+const CTALogoTwo = styled.img`
+  max-width: 600px;
+  margin-bottom: 20px;
+  display: inline-block;
+  vertical-align: bottom;
+  width: 100%;
+`;
+
+export default Login;
